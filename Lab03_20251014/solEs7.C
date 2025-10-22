6eaf13f360f102db01204b57a1571b939a13417d
Dario Tamascelli <Tama@Mac.lan>
2025-10-22 14:11:50 +0200

    lab04_final

--- Lab03_20251014/solEs7.C
+++ Lab03_20251014/solEs7.C
@@ -7,25 +7,7 @@ using namespace std;
 
 int main(){
 
-<<<<<<< HEAD
-    //Mi serve un contenitore per l'intero da tastiera
-    int appo;
-    
-    cout << endl <<"Inserire valore positivo: ";
-    cin >> appo;
-    
-    while(appo < 0){
-        cout << endl <<"Inserire valore positivo! : ";
-        cin >> appo;
-    }
-    //Qui arrivo solo se inserito valore positivo
-
-    cout << endl << "Valore positivo inserito: " << appo << endl;
-    
-    return 0;
-
-=======
-    int appo;
+   int appo;
 
     cout << endl << "Inserire valore positivo:";
     cin >> appo;
@@ -43,5 +25,4 @@ int main(){
     cout << endl <<"OK! Valore inserito:" << appo << endl;
 
     return 0;
->>>>>>> ab6ebe2a76f728eb2d923b457bcaf1c6a16de8ab
 }
