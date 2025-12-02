6030614fa616be15253b0a258d4b7e548b34acf3
Dario Tamascelli <Tama@TamaAirM4.local>
2025-12-02 10:44:22 +0100

    Lab09_init

--- Lab09_20251202/contaCaricaDati_fseek.C
+++ Lab09_20251202/contaCaricaDati_fseek.C
@@ -4,8 +4,51 @@
 #include <iostream>
 #include <fstream>
 
+using namespace std;
+
+
 int main(){
 
+    ifstream file_in;
+    double appo;
+    double *v;
+    int size;
+    int conta = 0;
+    int used=0;
+
+    file_in.open("data.dat");
+    if(file_in.fail()){
+        cout << endl << "Problema aggancio file input. Esco.";
+        return -1;
+    }
+
+    file_in >> appo;
+    while(!file_in.eof()){
+        //Elaboro dato
+        conta++;
+        file_in >> appo;
+    }
+
+    v =new double[conta];
+    size = conta;
 
 
+    file_in.clear();
+    file_in.seekg(0,ios::beg);
+
+    for(used=0; used<conta; used++)
+        file_in>>v[used];
+    
+    //Array caricato.
+    //Informazioni array aggiornate.
+    //Ora possiamo usarlo...
+
+
+    //Quando l'array non mi serve più, lo elimino.
+
+    delete [] v;
+    v = NULL;
+
+
+    return 0;
 }
